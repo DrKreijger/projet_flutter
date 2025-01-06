763d86207abbd0d520c3a53d6254417ee935e602
DrKreijger <sebdek7@gmail.com>
2025-01-06 19:08:55 +0100

    Update order model

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -3,6 +3,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Order {
   final String id;
   final String clientName;
+  final String clientAddress; // Nouvelle propriété : adresse du client
+  final String clientPhone; // Nouvelle propriété : numéro de téléphone du client
+  final String airport; // Nouvelle propriété : aéroport
+  final int numberOfPeople; // Nouvelle propriété : nombre de personnes
+  final double price; // Nouvelle propriété : prix
   final DateTime reservationDate;
   final DateTime departureDate;
   final String driverId;
@@ -11,6 +16,11 @@ class Order {
   Order({
     required this.id,
     required this.clientName,
+    required this.clientAddress,
+    required this.clientPhone,
+    required this.airport,
+    required this.numberOfPeople,
+    required this.price,
     required this.reservationDate,
     required this.departureDate,
     required this.driverId,
@@ -21,6 +31,11 @@ class Order {
     return Order(
       id: id,
       clientName: map['clientName'] ?? 'Inconnu',
+      clientAddress: map['clientAddress'] ?? '',
+      clientPhone: map['clientPhone'] ?? '',
+      airport: map['airport'] ?? '',
+      numberOfPeople: map['numberOfPeople'] ?? 0,
+      price: (map['price'] ?? 0).toDouble(),
       reservationDate: (map['reservationDate'] as Timestamp).toDate(), // Convertit Timestamp en DateTime
       departureDate: (map['departureDate'] as Timestamp).toDate(), // Convertit Timestamp en DateTime
       driverId: map['driverId'] ?? '',
@@ -31,6 +46,11 @@ class Order {
   Map<String, dynamic> toMap() {
     return {
       'clientName': clientName,
+      'clientAddress': clientAddress,
+      'clientPhone': clientPhone,
+      'airport': airport,
+      'numberOfPeople': numberOfPeople,
+      'price': price,
       'reservationDate': reservationDate,
       'departureDate': departureDate,
       'driverId': driverId,
